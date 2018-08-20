3d71c3c22a290da8b3cbf312ac456a2241ef7e3b
Alexander F. Rødseth <rodseth@gmail.com>
2018-08-20 15:25:53 +0200

    Add functions for clearing the pixel buffer

--- pixel.go
+++ pixel.go
@@ -9,3 +9,19 @@ import (
 func Pixel(pixels []uint32, x, y int32, c color.RGBA, pitch int32) {
 	pixels[y*pitch+x] = binary.BigEndian.Uint32([]uint8{c.A, c.R, c.G, c.B})
 }
+
+// Clear changes all pixels to the given color
+func Clear(pixels []uint32, c color.RGBA) {
+	colorValue := binary.BigEndian.Uint32([]uint8{c.A, c.R, c.G, c.B})
+	for i := range pixels {
+		pixels[i] = colorValue
+	}
+}
+
+// FastClear changes all pixels to the given uint32 color value,
+// like 0xff0000ff for: 0xff red, 0x00 green, 0x00 blue and 0xff alpha.
+func FastClear(pixels []uint32, colorValue uint32) {
+	for i := range pixels {
+		pixels[i] = colorValue
+	}
+}
