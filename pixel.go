c5659d210f0194186a7bd6c9055f3629faf628a2
Alexander F. Rødseth <rodseth@gmail.com>
2018-08-20 15:38:16 +0200

    Add functions for converting colors

--- pixel.go
+++ pixel.go
@@ -25,3 +25,16 @@ func FastClear(pixels []uint32, colorValue uint32) {
 		pixels[i] = colorValue
 	}
 }
+
+// ColorValueToRGBA converts from an uint32 to four bytes
+func ColorValueToRGBA(cv uint32) (uint8, uint8, uint8, uint8) {
+	bs := make([]uint8, 4)
+	binary.LittleEndian.PutUint32(bs, cv)
+	// r, g, b, a
+	return bs[0], bs[1], bs[2], bs[3]
+}
+
+// RGBAToColorValue converts from four bytes to an uint32
+func RGBAToColorValue(r, g, b, a uint8) uint32 {
+	return binary.BigEndian.Uint32([]uint8{a, r, g, b})
+}
