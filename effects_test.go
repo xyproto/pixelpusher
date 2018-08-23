5766972c728231807e8847b842b2b6e872344a41
Alexander F Rødseth <rodseth@gmail.com>
2018-08-23 17:45:06 +0200

    Add a test that fails

--- effects_test.go
+++ effects_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"runtime"
 	"testing"
+
+	"github.com/xyproto/pf"
 )
 
 func TestStretchContrast(t *testing.T) {
@@ -29,4 +31,52 @@ func TestStretchContrast(t *testing.T) {
 		fmt.Println(pixels)
 		t.Fail()
 	}
+}
+
+func TestStretchContrast2(t *testing.T) {
+	// Define an image, with red pixels that are not fully bright and not fully dark
+	pixels := make([]uint32, 320*200) // ARGB
+
+	pitch := int32(320) // 320 uint32s wide (1x3)
+	cores := runtime.NumCPU()
+
+	// Set a few bits to gray
+	pixels[1000] = 0xffc0c0c0
+	pixels[2000] = 0xffc0c0c0
+	pixels[42] = 0xffc0c0c0
+	pixels[256] = 0xffc0c0c0
+
+	// Turn on all the green bits
+	green := pf.OrGreen
+	pf.Map(cores, green, pixels)
+
+	pixels1 := make([]uint32, len(pixels))
+	pixels2 := make([]uint32, len(pixels))
+
+	copy(pixels1, pixels)
+	copy(pixels2, pixels)
+
+	// Stretch the contrast
+	StretchContrast(cores, pixels1, pitch, 0.9)
+
+	// Stretch the contrast, concurrently
+	StretchContrast2(cores, pixels2, pitch, 0.9)
+
+	// Check that they are equal
+	for i := range pixels {
+		if pixels1[i] != pixels2[i] {
+			fmt.Println("pixels1 and pixels2 differ at position", i)
+			fmt.Printf("pixels1[i]: %x, pixels2[i]: %x\n", pixels1[i], pixels2[i])
+			t.Fail()
+			return
+		}
+	}
+
+	// Check that the pixel at position 1000 is correct
+	//if pixels[1000] != 123 {
+	//	fmt.Println(pixels[1000])
+	//	t.Fail()
+	//	return
+	//}
+
 }
